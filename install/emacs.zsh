d2e5df1575ec8f2dd4b5b0732249d14c2a757647
Andrzej Sliwa <andrzej.sliwa@i-tool.eu>
2017-09-06 13:30:08 +0200

    fix: switched aliases names for emacs, fixed path for emacsclient.

--- install/emacs.zsh
+++ install/emacs.zsh
@@ -1,5 +1,6 @@
-alias ee='emacsclient -a "" -n'
-alias e='emacsclient -a ""'
+alias ee='emacsclient -a ""'
+alias e='emacsclient -a "" -n'
 export EDITOR='emacsclient -a ""'
 export PATH=~/.emacs.d/pairing:$PATH
 export PATH=/usr/local/Cellar/emacs-mac/emacs-25.2-z-mac-6.5/bin:$PATH
+export PATH=/usr/local/Cellar/emacs/25.2/bin:$PATH
