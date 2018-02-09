927e4922f12710ce0b09d86bd3e788a63a08fd00
Andrzej Sliwa <andrzej.sliwa@i-tool.eu>
2018-02-09 20:24:09 +0100

    re-enable modalka

---
+++ elpa/modalka-20171231.2213/modalka-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "modalka" "20171231.2213" "Easily introduce native modal editing of your own design" '((emacs "24.4")) :commit "e69ec8fa01e86cb789f7f2b27b6d5a47e1ca3069" :url "https://github.com/mrkkrp/modalka" :keywords '("modal" "editing"))
